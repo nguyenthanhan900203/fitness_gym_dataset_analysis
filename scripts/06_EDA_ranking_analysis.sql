935eb359ad55cda6540e9656d1b2129a6b00a6cf
Nhân Thành (Justin Nguyen) <105338015+nguyenthanhan900203@users.noreply.github.com>
2026-05-07 18:51:21 +0700

    Update 06_EDA_ranking_analysis.sql

--- scripts/06_EDA_ranking_analysis.sql
+++ scripts/06_EDA_ranking_analysis.sql
@@ -3,7 +3,7 @@
 Ranking Analysis
 ===============================================================================
 Purpose:
-    - To rank items (e.g., products, customers) based on performance or other metrics.
+    - To rank items based on performance or other metrics.
     - To identify top performers or laggards.
 
 SQL Functions Used:
