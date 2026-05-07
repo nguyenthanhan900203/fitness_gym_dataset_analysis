5ec8004f599951646966ec5893c877b2fd4f04c8
Nhân Thành (Justin Nguyen) <105338015+nguyenthanhan900203@users.noreply.github.com>
2026-05-07 17:29:54 +0700

    Update data_segmentation.sql

--- scripts/data_segmentation.sql
+++ scripts/data_segmentation.sql
@@ -4,8 +4,7 @@ Data Segmentation Analysis
 ===============================================================================
 Purpose:
     - To group data into meaningful categories for targeted insights.
-    - For customer segmentation, product categorization, or regional analysis.
-
+    - For customer segmentation.
 SQL Functions Used:
     - CASE: Defines custom segmentation logic.
     - GROUP BY: Groups data into segments.
